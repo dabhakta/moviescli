0d9e767fd61399396a23f6e823207ac31a68cec1
dabhakta <cypher2012@gmail.com>
2016-10-06 13:28:11 -0600

    Adding Project

---
+++ app/app.route.js
@@ -0,0 +1,14 @@
+"use strict";
+var router_1 = require("@angular/router");
+var directory_component_1 = require("./directory/directory.component");
+var home_component_1 = require("./home/home.component");
+var detail_component_1 = require("./detail/detail.component");
+var APP_ROUTES = [
+    { path: 'movies', component: directory_component_1.DirectoryComponent },
+    { path: 'detail/:id', component: detail_component_1.DetailComponent },
+    { path: '', component: home_component_1.HomeComponent }
+];
+exports.APP_ROUTES_PROVIDER = [
+    router_1.provideRouter(APP_ROUTES)
+];
+//# sourceMappingURL=app.route.js.map
